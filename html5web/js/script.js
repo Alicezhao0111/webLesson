315a584813c99d9d7fe8c76c0ea5e0c5d6fdab09
Alicezhao0111 <goodchild40723@gmail.com>
2023-05-18 15:47:18 +0800

    05/18 花室製作結束
    smoove

--- html5web/js/script.js
+++ html5web/js/script.js
@@ -28,10 +28,30 @@
 // 這邊的this =>.menu a
       var btn = $(this).attr("href"); /*取得"href"這個屬性(attr)*/ 
       var pos = $(btn).offset();/*取得正在按的那個按鈕的座標位置*/ 
-      $("html,body").animate({scrollTop:pos.top},1000); //1000是速度，若沒寫則為預設0.4秒
+      $("html,body").animate({scrollTop:pos.top},1500); //1500是速度，若沒寫則為預設0.4秒
 
 
      });
+
+     //滑動至頂
+     $('#gotop').click(function(){
+      $('html,body').animate({scrollTop:0},1500);
+     });
+
+     //置頂按鈕淡出淡入
+     $(window).scroll(function(){
+      //if(條件){執行動作} //加上stop所以不會一直等他執行完動作(如果一直滾就不會一直閃)
+      if($(this).scrollTop()>200){ //fadeTo比fadeIn穩定，但一定要給(速度,透明度)，速度不加可以給空值''
+        $('#gotop').stop().fadeTo('',1);
+      } else{
+        $('#gotop').stop().fadeOut();
+      }
+
+     });
+     //滑動載入 //第一個smoove是在html中自己取的class名
+     $('.smoove').smoove({
+      offset : '30%'
+  });
 });
 
 
