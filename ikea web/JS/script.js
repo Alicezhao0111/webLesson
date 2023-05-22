efabb375dbe23cd1b0e18dfe1936e82970dc4f51
Alicezhao0111 <goodchild40723@gmail.com>
2023-05-22 18:49:56 +0800

    20230522

---
+++ ikea web/JS/script.js
@@ -0,0 +1,19 @@
+//交換下拉選單定位
+$(window).scroll(function () {
+    if ($(this).scrollTop() == 0) {
+        $('.dropdown-menu, navbar-collapse').css("bottom", "100%");
+        $('.dropdown-menu, navbar-collapse').css("top", "auto");
+    }else{
+        $('.dropdown-menu, navbar-collapse').css("top", "100%");
+        $('.dropdown-menu, navbar-collapse').css("bottom", "auto");
+    }
+});
+
+
+$(window).scroll(function(){ /*抓卷軸的動作*/ 
+    var h = $(window).height()-400; /*視窗高度剪掉400後*/
+    if($(this).scrollTop()>h){
+        $('#food_1').css('animation-name','food_1'); 
+        $('#food_2').css('animation-name','food_2');
+    }
+    });
